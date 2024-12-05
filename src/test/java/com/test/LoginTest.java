013f0b7da2cd2949db4d1011c7e8b860d38beb01
satyashripisal22 <sdpisal229@gmail.com>
2024-12-05 22:48:42 +0530

    update Utility class

---
+++ src/test/java/com/test/LoginTest.java
@@ -0,0 +1,14 @@
+package com.test;
+
+import org.testng.annotations.Test;
+
+import com.utility.BaseClass;
+
+public class LoginTest extends BaseClass{
+
+	@Test
+	public void TC_001_verifyLogin() {
+		
+	}
+
+}
